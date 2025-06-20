8c1855e265950cc065099c05b9be561917d0e634
elihinton <a1923292@adelaide.edu.au>
2025-06-20 16:45:28 +0000

    [automatic commit] 2025-06-20 16:45:28 UTC 115.131.56.83

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -129,7 +129,6 @@ router.get('/dogs', async (req,res) => {
     });
   }
   const user_id = req.session.user.id;
-  console.log("id: ",user_id);
   try {
     const [result] = await db.query(
       `SELECT name, dog_id AS id FROM Dogs
