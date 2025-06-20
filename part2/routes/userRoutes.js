3ac58c0bd8d52c8b98bef348f282760e9e83607a
elihinton <a1923292@adelaide.edu.au>
2025-06-20 16:05:08 +0000

    [automatic commit] 2025-06-20 16:05:07 UTC 115.131.56.83

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -128,12 +128,17 @@ router.get('/dogs', async (req,res) => {
       message: "user session expired"
     });
   }
+  try {
+
+  } catch (e) {
+    
+  }
   const [result] = await db.query(
     `SELECT d.name FROM Dogs AS d
     INNER JOIN Users AS u ON u.user_id=d.owner_id
     WHERE u.user_id = ?`,
     [req.session.user.id]
   );
-})
+});
 
 module.exports = router;
