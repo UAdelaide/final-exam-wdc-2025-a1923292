ddaed404864b75fb814d201e2385fdbd24f4c00c
elihinton <a1923292@adelaide.edu.au>
2025-06-20 17:33:58 +0000

    [automatic commit] 2025-06-20 17:33:58 UTC 115.131.56.83

--- part2/app.js
+++ part2/app.js
@@ -29,18 +29,6 @@ app.use(session({
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-(async () => {
-    try {
-        const [result] = await db.query(`SELECT d.name AS dog_name, d.size AS size, u.username AS owner_username FROM Dogs AS d
-        INNER JOIN Users AS u ON d.owner_id=u.user_id;`);
-        return res.status(200).json(result);
-    } catch (e) {
-        return res.status(500).json({
-            message: `failed to query /api/dogs: ${e}`
-        });
-    }
-})();
-
 
 // Export the app instead of listening here
 module.exports = app;
