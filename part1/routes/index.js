1d577c84bc84e6b225d3b1d1b0860c207d80be2e
elihinton <a1923292@adelaide.edu.au>
2025-06-20 05:08:58 +0000

    [automatic commit] 2025-06-20 05:08:58 UTC 115.131.56.83

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -23,7 +23,8 @@ router.get('/api/dogs', async (req,res) => {
 router.get('/api/walkrequests/open', async (req,res) => {
   const [result] = await db.query(`SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests AS wr
   INNER JOIN Dogs AS d ON wr.dog_id=d.dog_id
-  INNER JOIN Users AS u ON `)
+  INNER JOIN Users AS u ON d.owner_id=u.user_id
+  WHERE wr.`)
 });
 
 router.get('/api/walkers/summary', async (req,res) => {
