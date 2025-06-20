2a4877a6f3e8f040032ef1c70352359379cbaecc
elihinton <a1923292@adelaide.edu.au>
2025-06-20 03:27:04 +0000

    [automatic commit] 2025-06-20 03:27:04 UTC 115.131.56.83

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -61,3 +61,19 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
   ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Rocky', 'large'),
   ((SELECT user_id FROM Users WHERE username = 'donaldtrump'), 'Booboo', 'small'),
   ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Luna', 'small');
+
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+  ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),
+   '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+
+  ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
+   '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+
+  ((SELECT dog_id FROM Dogs WHERE name = 'Rocky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),
+   '2025-06-11 10:00:00', 60, 'Mountain Trail', 'open'),
+
+  ((SELECT dog_id FROM Dogs WHERE name = 'Daisy' AND owner_id = (SELECT user_id FROM Users WHERE username = 'donaldtrump')),
+   '2025-06-12 07:45:00', 30, 'Downtown Park', 'open'),
+
+  ((SELECT dog_id FROM Dogs WHERE name = 'Luna' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
+   '2025-06-13 14:15:00', 20, 'Riverside Path', 'cancelled');
