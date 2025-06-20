cad629dd5c3f4ec70fc50df32da785bf539e61a3
elihinton <a1923292@adelaide.edu.au>
2025-06-20 05:16:26 +0000

    [automatic commit] 2025-06-20 05:16:25 UTC 115.131.56.83

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -38,6 +38,7 @@ router.get('/api/walkrequests/open', async (req,res) => {
 router.get('/api/walkers/summary', async (req,res) => {
   try {
     const [result] = await db.query();
+    res.status(200).json(result);
   } catch (e) {
     res.status(500).json({
       message: `request to /api/walkers/summary failed: ${e}`
