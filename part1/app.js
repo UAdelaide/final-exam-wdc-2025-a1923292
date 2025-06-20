eaff60d6f2bca34a1120905eeaa8312fd718758b
elihinton <a1923292@adelaide.edu.au>
2025-06-20 04:02:40 +0000

    [automatic commit] 2025-06-20 04:02:40 UTC 115.131.56.83

--- part1/app.js
+++ part1/app.js
@@ -51,7 +51,7 @@ const router = express.Router();
 
 router.get('/api/dogs', async () => {
   try {
-    await db.query(`SELECT d.`)
+    await db.query(`SELECT d.name, d.size, u.`)
   } catch (e) {
 
   }
