ef656364523d0f3289cd83ab1bc20134b0ad1f0b
elihinton <a1923292@adelaide.edu.au>
2025-06-20 04:29:41 +0000

    [automatic commit] 2025-06-20 04:29:41 UTC 115.131.56.83

--- part1/app.js
+++ part1/app.js
@@ -17,8 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-let db;
-
 (async () => {
 
   const connection = await mysql.createConnection({
