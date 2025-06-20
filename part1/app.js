b59956a8ae4553fa107bb62064da1446fb9be913
elihinton <a1923292@adelaide.edu.au>
2025-06-20 04:10:48 +0000

    [automatic commit] 2025-06-20 04:10:47 UTC 115.131.56.83

--- part1/app.js
+++ part1/app.js
@@ -53,9 +53,7 @@ router.get('/api/dogs', async (req,res) => {
   try {
     const result = await db.query(`SELECT d.name AS dog_name, d.size AS size, u.username AS owner_username FROM Dogs AS d
     INNER JOIN Users AS u ON d.owner_id=u.user_id;`);
-    return res.status(200).json({
-        
-    })
+    return res.status(200).json({})
   } catch (e) {
     return res.status(500).json({
         message: `failed to query /api/dogs: ${e}`
