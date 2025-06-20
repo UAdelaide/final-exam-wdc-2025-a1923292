d89c46dc42cdb255317740f6039dac6a76c9976e
elihinton <a1923292@adelaide.edu.au>
2025-06-20 03:46:18 +0000

    [automatic commit] 2025-06-20 03:46:18 UTC 115.131.56.83

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql = require('mysql2');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,10 +15,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+let db;
 
 (async () => {
 
-    
+
 
 })();
 
