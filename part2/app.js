1e89b44b822d4a27b694dcb9b2be5b7e931ce2d7
elihinton <a1923292@adelaide.edu.au>
2025-06-20 17:32:50 +0000

    [automatic commit] 2025-06-20 17:32:49 UTC 115.131.56.83

--- part2/app.js
+++ part2/app.js
@@ -30,14 +30,14 @@ app.use('/api/users', userRoutes);
 
 (() => {
     try {
-    const [result] = await db.query(`SELECT d.name AS dog_name, d.size AS size, u.username AS owner_username FROM Dogs AS d
-    INNER JOIN Users AS u ON d.owner_id=u.user_id;`);
-    return res.status(200).json(result);
-  } catch (e) {
-    return res.status(500).json({
-        message: `failed to query /api/dogs: ${e}`
-    });
-  }
+        const [result] = await db.query(`SELECT d.name AS dog_name, d.size AS size, u.username AS owner_username FROM Dogs AS d
+        INNER JOIN Users AS u ON d.owner_id=u.user_id;`);
+        return res.status(200).json(result);
+    } catch (e) {
+        return res.status(500).json({
+            message: `failed to query /api/dogs: ${e}`
+        });
+    }
 })();
 
 
